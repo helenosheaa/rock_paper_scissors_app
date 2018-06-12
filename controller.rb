ad4c3dc203affaf34c879b1eac36d59e364c5b31
helenosheaa <helenosheaa@gmail.com>
2018-06-12 16:04:49 +0100

    MVP complete

---
+++ controller.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/contrib/all')
+
+require_relative('./models/rock_paper_scissors')
+also_reload('./models/*')
+
+get ('/:hand1/:hand2') do
+  game = RockPaperScissors.new( params[:hand1], params[:hand2])
+  @result = game.play()
+  erb( :result )
+end
---
+++ models/rock_paper_scissors.rb
@@ -0,0 +1,31 @@
+class RockPaperScissors
+
+attr_reader(:hand1, :hand2)
+
+  def initialize(hand1, hand2)
+    @hand1 = hand1
+    @hand2 = hand2
+  end
+
+  def play
+    win = {
+      "rock" => "scissors",
+      "paper" => "rock",
+      "scissors" => "paper"
+    }
+    if
+       @hand1 == @hand2; return "it's a draw"
+    elsif
+       win[@hand1] == @hand2; return "#{@hand1} wins"
+    else
+      return "#{@hand2} wins"
+    end
+  end
+
+
+end
+
+# return "rock wins" if (@hand1 == "rock" && @hand2 == "scissors")
+# return "paper wins" if (@hand1 == "rock" && @hand2 == "paper")
+# return "scissors wins" if (@hand1 == "scissors" && @hand2 == "paper")
+# return "it's a draw"
