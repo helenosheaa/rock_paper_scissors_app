87c7b7a3afc8a201b0ee1581948b40da173cd6cc
helenosheaa <helenosheaa@gmail.com>
2018-06-12 16:39:39 +0100

    extensions completed

--- controller.rb
+++ controller.rb
@@ -8,6 +8,13 @@ get ('/') do
   erb(:home)
 end
 
+get ('/aigame/:hand1') do
+  aihand = ["rock", "paper", "scissors"]
+  game = RockPaperScissors.new( params[:hand1], aihand.sample)
+  @result = game.play()
+  erb( :result )
+end
+
 get ('/:hand1/:hand2') do
   game = RockPaperScissors.new( params[:hand1], params[:hand2])
   @result = game.play()
